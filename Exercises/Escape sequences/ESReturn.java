5bcdca543b043f527a5cef2480aaebd71466c366
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-02-17 22:12:18 +0800

    commit

--- Exercises/Escape sequences/ESReturn.java
+++ Exercises/Escape sequences/ESReturn.java
@@ -1,5 +1,5 @@
 public class ESReturn {
    public static void main ( String [] args) {
-      System.out.println("Good Morning Geeks! \r How are you all?  ");
+      System.out.println("Good Morning Geeks! \r How");
    }
 }
