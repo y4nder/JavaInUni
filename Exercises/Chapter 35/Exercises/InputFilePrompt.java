156dfff4f49757771060d20606d5760bbb70555e
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-03-28 19:32:48 +0800

    update chapter 35 exercise 1

--- Exercises/Chapter 35/Exercises/InputFilePrompt.java
+++ Exercises/Chapter 35/Exercises/InputFilePrompt.java
@@ -2,41 +2,41 @@
 import java.util.Scanner;
 public class InputFilePrompt{
    public static void main(String[] args){
-      String fileName, ext = "dat", extension, file = "";
+      String fileName, ext = "dat", extension;
+      boolean correctName = false;
       Scanner scan = new Scanner(System.in);
       
-      System.out.print("input file: ");
-      fileName = scan.nextLine();
-
-      int index = fileName.lastIndexOf('.');
-      
-      /*if(fileName.indexOf('.') == 0){
-         System.out.println("file name missng");
-      }*/
-      System.out.println(index);
-      if(index <= -1){
-         System.out.println("file name must end with .dat");
-         if()
-      }      
-      else{
-         extension = fileName.substring(index + 1);
-         fileName = fileName.substring(0, index );
+      do{
+         System.out.print("input file: ");
+         fileName = scan.nextLine();
+         int index = fileName.lastIndexOf('.');
          
-         if (extension.equals(ext)){
-            fileName = fileName.trim();
-            if (fileName.equals("")){
-               System.out.println("file name missing");
-            }
-            else if(file.contains(" ")){
-               System.out.println("file must not contain spaces");
+         if(index <= -1){
+            System.out.println(">>>file name must end with .dat<<<");
+         }      
+         else{
+            extension = fileName.substring(index + 1);
+            fileName = fileName.substring(0, index );
+            
+            if (extension.equals(ext)){
+               fileName = fileName.trim();
+               
+               if (fileName.equals("")){
+                  System.out.println(">>>file name missing<<<");
+               }
+               else if(fileName.contains(" ")){
+                  System.out.println(">>>file must not contain spaces<<<");
+               }
+               else{
+                  System.out.println("OK");
+                  correctName = true;
+               }            
             }
             else{
-               System.out.println("ok");
-            }            
-         }
-         else{
-            System.out.println("file name must end with .dat");
-         }
-      }      
+               System.out.println(">>>file name must end with .dat<<<");
+            }
+         }         
+      }
+      while(correctName == false);
    }
 }
