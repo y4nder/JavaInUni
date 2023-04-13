c869640421dfd4ff1e71052d8fc05875b66d59f3
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-04-14 04:58:12 +0800

    Password checker added

--- Exercises/Chapter 44/PasswordChecker.java
+++ Exercises/Chapter 44/PasswordChecker.java
@@ -2,14 +2,32 @@ import  java.util.Scanner;
 public class PasswordChecker{
    public static void main(String[] args){
       String password;
+      boolean passLength = false, hasUpperLowerCase = false, hasDigit = false;
       Scanner scan =  new Scanner(System.in);
       
-      System.out.println("Enter your password: ");
-      password = scan.nextLine();
-      
-      for(int i = 0; i < password.length(); i++){
-         char letter = password.charAt(i);
-         if(letter.equals(password.toUpperCase.chaAt(i) ) )
+      while( passLength == false || hasUpperLowerCase == false || hasDigit == false ){
+         System.out.println("Enter your password: ");
+         password = scan.nextLine();
+         
+         if( password.length() >= 7 ){
+            passLength = true;
+            
+            if( !password.equals( password.toUpperCase() ) && !password.equals( password.toLowerCase() ) ){
+               hasUpperLowerCase = true;
+               
+               for( int i = 0; i < password.length(); i++ ){
+                  if( Character.isDigit( password.charAt(i) ) ){
+                     hasDigit = true;
+                  }
+               }
+            }
+         }
+
+         if( passLength == false || hasUpperLowerCase == false || hasDigit == false ){
+            System.out.println("That password is not acceptable.\n");
+            passLength = false; hasUpperLowerCase = false; hasDigit = false;   
+         }
       }
+      System.out.println("Acceptable password.");
    }
 }
