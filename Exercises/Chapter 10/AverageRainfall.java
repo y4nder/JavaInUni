f82e0ac6ef9ce7001b53bec74107890224753c12
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-04-14 03:45:01 +0800

    fixed better docu and title applier

--- Exercises/Chapter 10/AverageRainfall.java
+++ Exercises/Chapter 10/AverageRainfall.java
@@ -5,15 +5,22 @@ public class AverageRainfall{
       byte april = 12, may = 14, june = 8;
       float rainfall = (april + may + june)/3.0F;
       
-      System.out.println("Rainfall for April: \t" + april);
-      System.out.println("Rainfall for May: \t" + may);
+      /*test comment*/
+      
+      /*
+         another test comment
+         another one
+      */
+      
+      System.out.println("Rainfall for April: \t" + april); /*
+      testing */System.out.println("Rainfall for May: \t" + may);
       System.out.println("Rainfall for June: \t" + june);
-      System.out.println("Average rainfall: \t" + rainfall);
+      System.out.println("Average rainfall: \t" + rainfall); //single comment test
    }
 }
 
 //if rainfall expression is (april+may+june)/3, result is 11.0
 //if rainfall expression (april+may+june)/3.0, result is 11.33333333333
 
-//the beginner's error was in the expression for computing the average, if it is divided by an integer it wil result as 0.0 
-               //but with a floating point number like 3.0 it will compute the result with decimals 
+/*the beginner's error was in the expression for computing the average, if it is divided by an integer it wil result as 0.0 
+               but with a floating point number like 3.0 it will compute the result with decimals 
--- Exercises/Chapter 44/BetterDocumentationPrinter.java
+++ Exercises/Chapter 44/BetterDocumentationPrinter.java
@@ -1,30 +1,40 @@
+//locations
+   //Z:/MM122/LubgubanL/jgrasp/Exercises/Chapter 10/AverageRainfall.java
+   //C:/Users/user/Documents/!school/2nd SEMESTER/COMPROG 2/Exercises/LubgubanL/Exercises/Chapter 10/AverageRainfall.java
 import java.io.*;
 import java.util.Scanner;
 public class BetterDocumentationPrinter{
    public static void main(String[] args)  throws FileNotFoundException{
       String line;
       boolean commentFound = false;
-      File file = new File("Z:/MM122/LubgubanL/jgrasp/Exercises/Chapter 10/AverageRainfall.java");      
+      File file = new File("C:/Users/user/Documents/!school/2nd SEMESTER/COMPROG 2/Exercises/LubgubanL/Exercises/Chapter 10/AverageRainfall.java");      
       Scanner scan = new Scanner(file);
       
       while(scan.hasNextLine() ){
          line = scan.nextLine();
          
+         if( commentFound == true && !line.contains("*/") ){
+            System.out.println( line.trim() );
+         }
+         
          if(line.contains("//") ){
-            int index = line.indexOf('/');
-            System.out.println(line.substring(index));        
+            System.out.println( line.substring( line.indexOf("//") ) );        
          }
          
-         if(line.contains("/*")){
-            commentFound = true;
+         if( line.contains("/*") ){
+            if( line.contains("*/") ){
+               System.out.println( line.substring( line.indexOf("/*"), line.indexOf("*/") + 2 ) );
+            }
+            else{
+               commentFound = true;
+               System.out.println( line.substring( line.indexOf("/*") ) );
+            }           
          }
-         
-         if(commentFound == true){
-            System.out.println(line);
-         }
-         
-         if(line.contains("*/")){
-            commentFound = false;
+         else{
+            if( line.contains("*/") ){
+               commentFound = false;
+               System.out.println( line.substring(0, line.indexOf("*/") + 2).trim() );
+            }
          }
       }
    }
