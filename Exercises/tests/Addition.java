a7fe9ec5f19adb7d12816a8119ef3e9175ed646d
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-04-28 10:38:01 +0800

    fdafdf

---
+++ Exercises/tests/Addition.java
@@ -0,0 +1,19 @@
+public class Addition {
+    private double a;
+    private double b;
+    private double sum;
+
+    public Addition(double a, double  b){
+        this.a = a;
+        this.b = b;
+        sum = add();
+    }
+    public double add(){
+        return a + b;
+    }
+
+    public String toString()
+    {
+      return "sum "  + sum;
+    }
+}
---
+++ Exercises/tests/testCalcu.java
@@ -0,0 +1,8 @@
+public class testCalcu{
+    public static void main(String[] args){
+        double a = 1, b = 2;
+
+        Addition addT = new Addition(a, b);
+        System.out.println(addT.toString() + "\n");
+    }
+}
