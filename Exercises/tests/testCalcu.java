42c14e6ed408601991981d6904d3738d9b65dd04
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-04-30 19:16:02 +0800

    changes new

--- Exercises/tests/testCalcu.java
+++ Exercises/tests/testCalcu.java
@@ -3,6 +3,10 @@ public class testCalcu{
         double a = 1, b = 2;
 
         Addition addT = new Addition(a, b);
-        System.out.println(addT.toString() + "\n");
+        Stringer st = new Stringer(addT);
+        System.out.println(addT+ "\n");
+
+        Subtraction subbT = new Subtraction(a, b);
+        System.out.println(subbT.toString() + "\n");
     }
 }
